fa2eb9944321382917ffc08eedef7b34cd1c0114
josedjaykv <josedjaykv@gmail.com>
2023-08-10 20:02:25 +0000

    first commit

--- main.cpp
+++ main.cpp
@@ -3,23 +3,47 @@
 
 #include "mbed.h"
 #include <iostream>
+#include <numeric>
+using namespace std;
 
+int gcd(int a, int b) {
+  while (b != 0) {
+    int temp = a % b;
+    a = b;
+    b = temp;
+  }
+  return a;
+}
+
+int mcd3(int a, int b, int c) {
+  return gcd(gcd(a, b), c);
+}
 
 // Periodo de parpadeo máximo común divisor - MCD - para los LEDs del LED RGB  
-#define BLINKING_PERIOD     20
 #define BLINKING_UNITS      "ms"
 #define BLINKING_RATE       20ms    // Falta concatenación de BLINKING_PERIOD y BLINKING_UNITS 
-// Periodos de parpadeo adimensionalizados de los LEDs: Tx = LEDx_PERIOD*BLINKING_RATE 
-#define LEDR_PERIOD         5       
-#define LEDG_PERIOD         7       
-#define LEDB_PERIOD         11      
 // Objeto para establecer la comunicación serial con el Pc
-BufferedSerial serial(USBTX, USBRX, 9600);
-using namespace std;
+//BufferedSerial serial(USBTX, USBRX, 9600);
+UnbufferedSerial serial(USBTX, USBRX, 9600);
+
 
 
 int main()
 {
+    cout << "Frecuencia del led Rojo" << "\n" << "Por favor ingrese un valor:" << "\n";
+   int R;
+   cin >> R; 
+
+   cout << "Frecuencia del led Rojo" << "\n" << "Por favor ingrese un valor:" << "\n";
+   int G;
+   cin >> G; 
+
+   cout << "Frecuencia del led Rojo" << "\n" << "Por favor ingrese un valor:" << "\n";
+   int B;
+   cin >> B; 
+
+    int mcd = mcd3(R,G,B);
+
     // Initialise the digital pin LED1 as an output
     DigitalOut ledR(LED1, 1);
     DigitalOut ledG(LED2, 1);
@@ -28,17 +52,17 @@ int main()
     int count = 0;
 
     while (true) {
-        if(count % 5 == 0) {
+        if(count % (R/mcd) == 0) {
             ledR = !ledR;
-            cout << "LedR = " << count*BLINKING_PERIOD << BLINKING_UNITS << endl;
+            cout << "LedR = " << count*mcd << BLINKING_UNITS << endl;
         }
-        if(count % 7 == 0) {
+        if(count % (G/mcd) == 0) {
             ledG = !ledG;
-            cout << "LedG = " << count*BLINKING_PERIOD << BLINKING_UNITS << endl;
+            cout << "LedG = " << count*mcd << BLINKING_UNITS << endl;
         }
-        if(count % 11 == 0) {
+        if(count % (B/mcd) == 0) {
             ledB = !ledB;
-            cout << "LedB = " << count*BLINKING_PERIOD << BLINKING_UNITS << endl;
+            cout << "LedB = " << count*mcd << BLINKING_UNITS << endl;
         }
 
         ThisThread::sleep_for(BLINKING_RATE);
